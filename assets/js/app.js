2d9cf14396c270bac3aae952437daf0cfe41f851
John Murray <jmurray@redventures.com>
2015-02-04 01:26:51 -0500

    stashing a copy of my desired nav layout so i can come back later

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,11 @@ App = Ember.Application.create();
 App.Router.map(function(){
 	this.resource('nav');
 	this.resource('add');
+	this.resource('view');
+});
+
+App.NavRoute = Ember.Route.extend({
+	renderTemplate: function() {
+		this.render({ controller: 'nav' });
+	}
 });

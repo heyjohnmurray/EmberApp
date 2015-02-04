1dec233c0fa109fc6a1a457b9909966dd67b74a6
John Murray <jmurray@redventures.com>
2015-02-04 01:08:59 -0500

    finally figured out that app.min.js was what was causing the problem

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,5 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-  	this.route('add');
-});
-
-App.AddRoute = Ember.Route.extend({
-	model: function(){
-		return add;
-	}
+	this.resource('about');
 });

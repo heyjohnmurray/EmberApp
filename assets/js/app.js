f0f52be59742aad10782be1a8f9752ea98b0c696
John Murray <jmurray@redventures.com>
2015-02-04 23:59:10 -0500

    big button nav only shows on index page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,3 @@ App.Router.map(function(){
 	this.resource('add');
 	this.resource('view');
 });
-
-App.IndexController = Ember.ObjectController.extend({
-	isVisible : false,
-
-	actions: {
-		show: function(){
-			this.set('isVisible', true);
-		},
-
-		hide: function(){
-		  	this.set('isVisible', false);
-		}
-	},
-});

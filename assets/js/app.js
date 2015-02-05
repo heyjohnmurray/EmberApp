287ce2e972f6688719aaca01ecfaf89f20aa28a8
John Murray <jmurray@redventures.com>
2015-02-04 23:30:43 -0500

    got working example of visibility toggle working. this will hopefully help with my nav toggle issue.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,20 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-	this.resource('nav');
 	this.resource('add');
 	this.resource('view');
+});
+
+App.IndexController = Ember.ObjectController.extend({
+	isVisible : false,
+
+	actions: {
+		show: function(){
+			this.set('isVisible', true);
+		},
+
+		hide: function(){
+		  	this.set('isVisible', false);
+		}
+	},
 });
